aad08044687971c3e84efb7d6a8195eb1c605fe7
andersgeriksen <andersge92@gmail.com>
2020-05-20 15:37:55 +0200

    fixed numerical memoryloss

--- project1/src/Hamiltonians/hamiltonian.cpp
+++ project1/src/Hamiltonians/hamiltonian.cpp
@@ -13,38 +13,27 @@ double Hamiltonian::numeric()
     double h = 0.001;
     double h2 = 1e6;
     std::vector<class Particle*> r = m_system->getParticles();
-    std::vector<class Particle*> rplus;
-    std::vector<class Particle*> rminus;
-    for( int i=0; i<m_system->getNumberOfParticles(); i++ )
-    {
-        rplus.push_back(new Particle());
-        rplus.at(i)->setNumberOfDimensions(m_system->getNumberOfDimensions());
-        rplus.at(i)->setPosition(r[i]->getPosition());
-        rminus.push_back(new Particle());
-        rminus.at(i)->setNumberOfDimensions(m_system->getNumberOfDimensions());
-        rminus.at(i)->setPosition(r[i]->getPosition());
-    }
+
     double psiplus = 0; double psiminus = 0;
     double psicurrent = m_system->getWaveFunction()->evaluate(r);
     double kinetic = 0; 
+
     for( int part=0; part < m_system->getNumberOfParticles(); part++ )
     {
         for( int dim=0; dim < m_system->getNumberOfDimensions(); dim++ )
         {
-            rplus[part]->adjustPosition(h, dim);
-            rminus[part]->adjustPosition(-h, dim);
-            psiplus = m_system->getWaveFunction()->evaluate(rplus);
-            psiminus = m_system->getWaveFunction()->evaluate(rminus);
+            r[part]->adjustPosition(h, dim); 
+            psiplus = m_system->getWaveFunction()->evaluate(r);
+            r[part]->adjustPosition(-h,dim);
+
+            r[part]->adjustPosition(-h, dim); 
+            psiminus = m_system->getWaveFunction()->evaluate(r);
+            r[part]->adjustPosition(h,dim);
+
             kinetic -= ( psiminus + psiplus - 2*psicurrent );
-            rplus[part]->adjustPosition(-h, dim);
-            rminus[part]->adjustPosition(h, dim);
         }//dimensions
     }//particles
     //std::cout << "finished numeric differentiation" << std::endl;
     //std::cout << "--------------------------------" << std::endl;
-    //rplus.clear(); 
-    std::vector<class Particle*>().swap( rplus );
-    //rminus.clear();
-    std::vector<class Particle*>().swap( rminus );
     return 0.5*h2*kinetic/psicurrent;
 }
